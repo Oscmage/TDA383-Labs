972b8bdd74e3277d5427e2883da6aa1d06b1de1d
michelle-tl <miss_amtl@live.com>
2017-01-19 08:14:42 +0100

    test

--- src/Lab1.java
+++ src/Lab1.java
@@ -2,15 +2,21 @@ import TSim.*;
 
 public class Lab1 {
 
-  public Lab1(Integer speed1, Integer speed2) {
-    TSimInterface tsi = TSimInterface.getInstance();
+    public Lab1(Integer speed1, Integer speed2) {
+        TSimInterface tsi = TSimInterface.getInstance();
 
-    try {
-      tsi.setSpeed(1,speed1);
+        try {
+            tsi.setSpeed(1, speed1);
+            tsi.setSpeed(2, speed2);
+            System.out.println("Sensor XPOS: " + tsi.getSensor(1).getXpos());
+            tsi.setSpeed(1,0);
+        } catch (CommandException e) {
+            e.printStackTrace();    // or only e.getMessage() for the error
+            System.exit(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    catch (CommandException e) {
-      e.printStackTrace();    // or only e.getMessage() for the error
-      System.exit(1);
-    }
-  }
+
+
 }
