f5b32d938e205afe9920e330edd055603857e441
Oscar Evertsson <oscar.evertsson@live.com>
2017-01-19 09:23:59 +0100

    OUR DAMN FIRST ATTEMPT

--- src/Lab1.java
+++ src/Lab1.java
@@ -5,16 +5,21 @@ import java.util.concurrent.Semaphore;
 public class Lab1 {
     private TSimInterface tsi;
     private Semaphore s1;
-    private Direction t1;
-    private Direction t2;
+    private Direction t1,t2;
+    private int speed1,speed2;
 
     public Lab1(Integer speed1, Integer speed2) {
         tsi = TSimInterface.getInstance();
         s1 = new Semaphore(1, true);
+        this.speed1 = speed1;
+        this.speed2 = speed2;
 
         try {
             tsi.setSpeed(1, speed1);
             tsi.setSpeed(2, speed2);
+            tsi.setSwitch(17,7,TSimInterface.SWITCH_RIGHT);
+            tsi.setSwitch(15,9,TSimInterface.SWITCH_RIGHT);
+            tsi.setSwitch(3,11,TSimInterface.SWITCH_RIGHT);
             t1 = Direction.DOWN;
             t2 = Direction.UP;
             listenEvent();
@@ -44,13 +49,17 @@ public class Lab1 {
 
     private void handleEvent(SensorEvent e) {
         int x = e.getXpos();
-        int y = e.getXpos();
-        if (x == 15 && y == 13) {
+        int y = e.getYpos();
+        if (x == 10 && y == 13) {
             try {
                 tsi.setSpeed(e.getTrainId(),0);
+                Thread.sleep(1000 + (20 * Math.abs(speed1)));
                 t1 = Direction.UP;
                 tsi.setSpeed(e.getTrainId(),-15);
+                speed1 = -15;
             } catch (CommandException e1) {
+                e1.printStackTrace();
+            } catch (InterruptedException e1) {
                 e1.printStackTrace();
             }
         }
