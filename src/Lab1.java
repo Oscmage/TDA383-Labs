f68d90719867b5be85abac5377eb6f11e93b5060
Oscar Evertsson <oscar.evertsson@live.com>
2017-01-19 09:05:57 +0100

    Added some base code.

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,20 +1,58 @@
 import TSim.*;
 
+import java.util.concurrent.Semaphore;
+
 public class Lab1 {
+    private TSimInterface tsi;
+    private Semaphore s1;
+    private Direction t1;
+    private Direction t2;
 
     public Lab1(Integer speed1, Integer speed2) {
-        TSimInterface tsi = TSimInterface.getInstance();
+        tsi = TSimInterface.getInstance();
+        s1 = new Semaphore(1, true);
 
         try {
             tsi.setSpeed(1, speed1);
             tsi.setSpeed(2, speed2);
-            System.out.println("Sensor XPOS: " + tsi.getSensor(1).getXpos());
+            t1 = Direction.DOWN;
+            t2 = Direction.UP;
+            listenEvent();
             tsi.setSpeed(1,0);
         } catch (CommandException e) {
             e.printStackTrace();    // or only e.getMessage() for the error
             System.exit(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        }
+    }
+
+    private enum Direction {
+        UP,DOWN
+    }
+
+    private void listenEvent() {
+        while (true) {
+            try {
+                SensorEvent e = tsi.getSensor(1);
+                handleEvent(e);
+            } catch (CommandException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleEvent(SensorEvent e) {
+        int x = e.getXpos();
+        int y = e.getXpos();
+        if (x == 15 && y == 13) {
+            try {
+                tsi.setSpeed(e.getTrainId(),0);
+                t1 = Direction.UP;
+                tsi.setSpeed(e.getTrainId(),-15);
+            } catch (CommandException e1) {
+                e1.printStackTrace();
+            }
         }
     }
 
