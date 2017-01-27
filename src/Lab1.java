97d5528f4ebee57c5b38d44c7aec9d42915ddd87
michelle-tl <miss_amtl@live.com>
2017-01-27 13:29:22 +0100

    last version

--- src/Lab1.java
+++ src/Lab1.java
@@ -2,8 +2,6 @@ import TSim.CommandException;
 import TSim.SensorEvent;
 import TSim.TSimInterface;
 
-import java.awt.*;
-import java.util.HashMap;
 import java.util.concurrent.Semaphore;
 
 public class Lab1 {
@@ -158,21 +156,15 @@ public class Lab1 {
 
 
 //                    arrives to "single path" 14 7 14 8 19 8
-                } else if (this.direction == Direction.UP && (x == 14 && y == 7)) {
-
-                } else if (this.direction == Direction.UP && (x == 14 && y == 8)) {
-
                 } else if (this.direction == Direction.DOWN && (x == 14 && y == 7)) {
-                    stopAtIntersectionTryAcquire(2);
-                    setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
-                    setSpeed(speed);
+                    stopSwitchGo(2, 17, 7, TSimInterface.SWITCH_RIGHT);
+
                     SensorEvent e1 = getActiveSensor();
                     release(semaphores[0]);
                     handleEvent(e1);
                 } else if (this.direction == Direction.DOWN && (x == 14 && y == 8)) {
-                    stopAtIntersectionTryAcquire(2);
-                    setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
-                    setSpeed(speed);
+                    stopSwitchGo(2, 17, 7, TSimInterface.SWITCH_LEFT);
+
 
                 } else if (this.direction == Direction.UP && (x == 19 && y == 8)) {
                     if (!tryAcquire(semaphores[0])) {
@@ -195,35 +187,24 @@ public class Lab1 {
 
                     //middle right
                 }  else if (this.direction == Direction.UP && (x == 13 && y == 9)) {
-                    stopAtIntersectionTryAcquire(2);
-                    setSpeed(speed);
-                    setSwitch(15,9, TSimInterface.SWITCH_RIGHT);
+                    stopSwitchGo(2, 15, 9, TSimInterface.SWITCH_RIGHT);
+
                     SensorEvent e1 = getActiveSensor();
                     release(semaphores[3]);
                     handleEvent(e1);
                 } else if (this.direction == Direction.UP && (x == 13 && y == 10)) {
-                    stopAtIntersectionTryAcquire(2);
-                    setSpeed(speed);
-                    setSwitch(15,9, TSimInterface.SWITCH_LEFT);
-                } else if (this.direction == Direction.DOWN && (x == 13 && y == 9)) {
-
-                } else if (this.direction == Direction.DOWN && (x == 13 && y == 10)) {
-
+                    stopSwitchGo(2, 15, 9, TSimInterface.SWITCH_LEFT);
 
                     //middle left
-                } else if (this.direction == Direction.UP && (x == 6 && y == 9)) {
-                } else if (this.direction == Direction.UP && (x == 6 && y == 10)) {
                 } else if (this.direction == Direction.DOWN && (x == 6 && y == 9)) {
-                    stopAtIntersectionTryAcquire(4);
-                    setSwitch(4,9, TSimInterface.SWITCH_LEFT);
-                    setSpeed(speed);
+                    stopSwitchGo(4, 4, 9, TSimInterface.SWITCH_LEFT);
+
+
                     SensorEvent e1 = getActiveSensor();
                     release(semaphores[3]);
                     handleEvent(e1);
                 } else if (this.direction == Direction.DOWN && (x == 6 && y == 10)) {
-                    stopAtIntersectionTryAcquire(4);
-                    setSwitch(4,9, TSimInterface.SWITCH_RIGHT);
-                    setSpeed(speed);
+                    stopSwitchGo(4, 4, 9, TSimInterface.SWITCH_RIGHT);
 
                     //left sensor
                 } else if (this.direction == Direction.UP && (x == 1 && y == 10)) {
@@ -246,22 +227,14 @@ public class Lab1 {
                     handleEvent(e1);
 
                     //last sensors
-                }else if (this.direction == Direction.DOWN && (x == 5 && y == 11)) {
-                }else if (this.direction == Direction.DOWN && (x == 3 && y == 13)) {
-
                 }else if (this.direction == Direction.UP && (x == 5 && y == 11)) {
-                    stopAtIntersectionTryAcquire(4);
-                    setSwitch(3,11, TSimInterface.SWITCH_LEFT);
-                    setSpeed(speed);
+                    stopSwitchGo(4, 3, 11, TSimInterface.SWITCH_LEFT);
+
                     SensorEvent e1 = getActiveSensor();
                     release(semaphores[5]);
                     handleEvent(e1);
                 }else if (this.direction == Direction.UP && (x == 3 && y == 13)) {
-                    stopAtIntersectionTryAcquire(4);
-                    setSwitch(3,11, TSimInterface.SWITCH_RIGHT);
-                    setSpeed(speed);
-
-
+                    stopSwitchGo(4, 3, 11, TSimInterface.SWITCH_RIGHT);
                 }
 
             }
@@ -280,6 +253,12 @@ public class Lab1 {
             acquire(semaphores[i]);
         }
 
+        private void stopSwitchGo(int semaphore, int switchX, int switchY, int direction){
+            stopAtIntersectionTryAcquire(semaphore);
+            setSwitch(switchX, switchY, direction);
+            setSpeed(speed);
+        }
+
         private SensorEvent getActiveSensor(){
             try {
                 SensorEvent next = tsi.getSensor(this.id);
@@ -296,80 +275,6 @@ public class Lab1 {
             return null;
         }
 
-//        private boolean handleStationDown(SensorEvent e) {
-//            int y = e.getYpos();
-//            int x = e.getXpos();
-//
-//            Semaphore s = hashPoint.get(new Point(x,y));
-//
-//            if (x == 6 && y == 9) {
-//
-//                if (this.direction == Direction.DOWN) {
-//                    setSpeed(0);
-//                    acquire(s);
-//                    setSpeed(speed);
-//                    setSwitch(4,9,TSimInterface.SWITCH_RIGHT);
-//                    Semaphore whichStation = hashPoint.get(new Point(3,13));
-//
-//                    if (whichStation.availablePermits() == 0) { // You should go up
-//                        //The station farthest down contains a train
-//                        setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
-//                    } else { // You should go down
-//                        acquire(whichStation);
-//                        setSwitch(3 , 11, TSimInterface.SWITCH_LEFT);
-//                    }
-//                } else {
-//                    release(s);
-//                }
-//                return true;
-//            }
-//
-//            if (x == 6 && y == 10) {
-//
-//                if (this.direction == Direction.DOWN) {
-//                    setSpeed(0);
-//                    acquire(s);
-//                    setSpeed(speed);
-//                    setSwitch(4,9,TSimInterface.SWITCH_LEFT);
-//                    Semaphore onDownMiddle = hashPoint.get(new Point(13,10));
-//                    onDownMiddle.release();
-//                    Semaphore whichStation = hashPoint.get(new Point(3,13));
-//
-//                    if (whichStation.availablePermits() == 0) { // You should go up
-//                        //The station farthest down contains a train
-//                        setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
-//                    } else { // You should go down
-//                        acquire(whichStation);
-//                        setSwitch(3, 11, TSimInterface.SWITCH_LEFT);
-//                    }
-//                } else {
-//                    s.release();
-//                }
-//                return true;
-//            }
-//
-//            if (x == 5 && y == 11) {
-//                    if (this.direction == Direction.DOWN) {
-//                        s.release();
-//                    } else {
-//                        setSpeed(0);
-//                        acquire(s);
-//                        setSwitch(3,11, TSimInterface.SWITCH_LEFT);
-//                        setSpeed(speed);
-//                        Semaphore whichRail = hashPoint.get(new Point(13,10));
-//                        if (whichRail.availablePermits() == 0) { // You should go up
-//                            //The station farthest down contains a train
-//                            setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
-//                        } else { // You should go down
-//                            acquire(whichRail);
-//                            setSwitch(4 , 9, TSimInterface.SWITCH_LEFT);
-//                        }
-//                    }
-//
-//            }
-//            return false;
-//        }
-
         private void setSwitch(double x, double y, int direction) {
             try {
                 tsi.setSwitch((int) x,(int) y,direction);
@@ -377,19 +282,6 @@ public class Lab1 {
                 e1.printStackTrace();
             }
         }
-
-//        private boolean handleStartStop(SensorEvent e) {
-//            double y = e.getYpos();
-//            double x = e.getXpos();
-//            if (x == 15 && (y == 3 || y == 5)) {
-//                if (this.direction == Direction.UP) slowDownAndSwitchDirection(e);
-//                return true;
-//            } else if (x == 14 && (y == 11 || y == 13)) {
-//                if (this.direction == Direction.DOWN) slowDownAndSwitchDirection(e);
-//                return true;
-//            }
-//            return false;
-//        }
 
         private void slowDownAndSwitchDirection() {
             setSpeed(0);
@@ -399,25 +291,6 @@ public class Lab1 {
             if (this.direction == Direction.DOWN) this.direction = Direction.UP;
             else this.direction = Direction.DOWN;
         }
-
-//        private boolean handleCross(SensorEvent e) {
-//            int x = e.getXpos();
-//            int y = e.getYpos();
-//            Semaphore s = hashPoint.get(new Point(x, y));
-//
-//            if (x == 6 && y == 6 || x == 9 && y == 5) {
-//                if (this.direction == Direction.UP) s.release();
-//                else acquire(s);
-//                return true;
-//            }
-//
-//            if (x == 10 && (y == 7 || y == 8)) {
-//                if (this.direction == Direction.DOWN) s.release();
-//                else acquire(s);
-//                return true;
-//            }
-//            return false;
-//        }
 
         private void acquire(Semaphore s) {
             try {
@@ -435,45 +308,10 @@ public class Lab1 {
             s.release();
         }
 
-//        private void handlePath(SensorEvent e) {
-//            int x = e.getXpos();
-//            int y = e.getYpos();
-//            Semaphore s = hashPoint.get(new Point(x, y));
-//            System.out.println("In handlePath. Id: " + this.id + " x:" + x + " y:" + y + " ");
-//
-//            if (isSensorActive(e)) {
-//                if ((x == 14 && y == 7) || x == 13 && y == 9) {
-//                    setSpeed(0);
-//                    System.out.println("Semaphore acc... for id: " + this.id);
-//                    acquire(s);
-//                    System.out.println("Semaphore acquired for id: " + this.id);
-//                    setSpeed(speed);
-//
-//                    if(this.direction == Direction.UP){
-//                        setSwitch(15,9, TSimInterface.SWITCH_RIGHT);
-//                    }else{
-//                        setSwitch(15,9, TSimInterface.SWITCH_LEFT);
-//
-//                    }
-//
-//                }
-//
-//                if (direction == Direction.DOWN && (x == 13 && y == 10)) {
-//                    System.out.println("Semaphore re.... for id: " + this.id);
-//                    Semaphore s1 = hashPoint.get(new Point(13,9));
-//                    release(s1);
-//                    System.out.println("Semaphore released for id: " + this.id);
-//
-//                }
-//            }
-//
-//        }
-
         private boolean isSensorActive(SensorEvent e) {
             return e.getStatus() == 1;
         }
 
     }
 
-    //6 semaphorer behövs
 }
