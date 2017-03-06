859f503be6967a17d97a858c889b50ab058aadb1
Oscar Evertsson <oscar.evertsson@live.com>
2017-03-06 21:06:25 +0100

    Added more comments.

--- Lab4/server.erl
+++ Lab4/server.erl
@@ -55,18 +55,18 @@ handle(St, {join,Channel,PID}) ->
 %% Handle-function done, add the job that is recently done to our results-list
 % Then it checks if there are more jobs left by checking if the currentResultList is equal to refs-list
 % If there are no more jobs left, it sorts the ResultList and sends back {done, SortedList and current Ref} back to Pid
-% if there are more jobs left, it will add the current finished job to results-list    
+% if there are more jobs left, it will add the current finished job to results-list
 handle(St, {done, Result, Ref}) ->
   CurrentResultList = St#server_st.results ++ [{Ref,Result}],
   if
-      length(CurrentResultList) == length(St#server_st.refs) ->
-        SortedList = sort_by_ref(CurrentResultList, St#server_st.refs),
-        Pid = element(2, St#server_st.respond_to),
-        R = element(1, St#server_st.respond_to),
-        Pid ! {done, SortedList, R},
-        {reply, ok, St#server_st{ respond_to={}, refs=[], results=[]}};
+      length(CurrentResultList) == length(St#server_st.refs) -> % We got responses from all workers?
+        SortedList = sort_by_ref(CurrentResultList, St#server_st.refs), % Sort the list based on refs.
+        Pid = element(2, St#server_st.respond_to), % 'respond_to' is on format {Ref,Pid}
+        R = element(1, St#server_st.respond_to), % R = Ref.
+        Pid ! {done, SortedList, R}, % Send the reponse to whoever asked for job to be done.
+        {reply, ok, St#server_st{ respond_to={}, refs=[], results=[]}}; % We're now done with the job, so reset variables
       true ->
-        {reply, ok, St#server_st{results = CurrentResultList}}
+        {reply, ok, St#server_st{results = CurrentResultList}} % Not done, just update state
   end;
 
 %% This handle-function is called by send_job in cchat.erl
@@ -76,14 +76,14 @@ handle(St, {done, Result, Ref}) ->
 handle(St, {send_job, F, Args, Ref, Pid}) ->
   Users = get_all_users(St),
   case Users of
-    [] ->
+    [] -> % we got no workers
       {reply,no_users,St};
     _ -> % Great, we got some users that can do some work for us
-      List_TaskUser = assign_tasks(Users, Args),
-      Refs = [ Temp_Ref || {_, _, Temp_Ref} <-  List_TaskUser],
+      List_TaskUser = assign_tasks(Users, Args), %Assign tasks to users
+      Refs = [ Temp_Ref || {_, _, Temp_Ref} <-  List_TaskUser], % Get the Refs
       P = self(),
-      [ spawn (fun() -> genserver:request(User, {do_task, Task, F, Temp_Ref, P}, infinity) end) || {User, Task, Temp_Ref} <- List_TaskUser],
-      {reply, ok, St#server_st{refs = Refs, respond_to = {Ref, Pid} } }
+      [ spawn (fun() -> genserver:request(User, {do_task, Task, F, Temp_Ref, P}, infinity) end) || {User, Task, Temp_Ref} <- List_TaskUser], % Spread the tasks to the users.
+      {reply, ok, St#server_st{refs = Refs, respond_to = {Ref, Pid} } } % Update state so we know who to reply to
   end.
 
 %% Sort a list based on the second argument, Refs
